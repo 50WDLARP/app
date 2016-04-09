4717bf296295112c4566307ddcf9d2040207d1ae
Erica Schwartz <Erica_S.Schwartz@tufts.edu>
2016-04-09 17:53:11 -0400

    made layouts

---
+++ SonicIOS/Views/SonicView.swift
@@ -0,0 +1,25 @@
+//
+//  SonicView.swift
+//  Cool Beans
+//
+//  Created by Kyle on 11/14/14.
+//  Copyright (c) 2014 Kyle Weiner. All rights reserved.
+//
+
+import UIKit
+
+class SonicView: UIView {
+
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distance : UILabel!
+
+    // MARK: Lifecycle
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let bundle = NSBundle(forClass: self.dynamicType)
+        self.addSubview(bundle.loadNibNamed("SonicView", owner: self, options: nil)[0] as! UIView)
+    }
+
+}
